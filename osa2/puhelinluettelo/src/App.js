513f4016df7034f00e132de796fd74e6b6972965
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2021-09-05 22:28:20 +0300

    complete part 2.11.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import ContactForm from './components/ContactForm'
 import ListDisplay from './components/ListDisplay'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNameFilter, setNewNameFilter ] = useState('')
   const [ newNumber, setNewNumber ] = useState(0)
-  const [ personsToDisplay, setPersonsToDisplay ] = useState([...persons])
+  const [ personsToDisplay, setPersonsToDisplay ] = useState([])
+  
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons([...response.data])
+      setPersonsToDisplay([...response.data])
+    })
+  }, [])
 
   const nameChange = (e) => {
     e.preventDefault()
