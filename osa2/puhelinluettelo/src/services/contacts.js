8b667a4b16fd33c45c88e42c1d114e75ed241cab
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2021-10-10 14:13:50 +0300

    update api url

--- osa2/puhelinluettelo/src/services/contacts.js
+++ osa2/puhelinluettelo/src/services/contacts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://murmuring-mountain-11390.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
