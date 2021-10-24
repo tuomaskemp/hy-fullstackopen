af35aa414c81d6b2618c3b499ce7f4fc5e4e6320
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2021-10-24 23:20:16 +0300

    complete 4.5.

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, current) => acc + current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const ascBlogs = blogs.sort((first, second) => first.likes - second.likes)
+    return ascBlogs[ascBlogs.length - 1]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
