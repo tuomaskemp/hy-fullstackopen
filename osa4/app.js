0d468fcc59f0554f96de1f25b175fd99cb474893
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2021-10-24 21:58:33 +0300

    complete 4.1.-4.2.

---
+++ osa4/app.js
@@ -0,0 +1,16 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+module.exports = app
