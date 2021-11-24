046c3c4e732bf1ed2601411a5ec9e40faee63235
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2021-11-25 00:13:18 +0200

    complete 7.1. -7.3.

---
+++ osa7/routed-anecdotes/src/components/SingleAnecdote.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SingleAnecdote = ({ anecdote }) => {
+    return (
+        <div>
+            <h1>{anecdote.content} by {anecdote.author}</h1>
+            <p>has {anecdote.votes} votes</p>
+            <p>for more info see: <a href={anecdote.info}>{anecdote.info}</a></p>
+        </div>
+    )
+}
+
+export default SingleAnecdote
