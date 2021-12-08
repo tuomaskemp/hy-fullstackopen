8d5dfe261610114bdaffe0383808c54b199c3bdb
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2021-12-08 13:50:08 +0200

    complete 8.12.

--- osa8/library-frontend/src/components/UpdateBirthyear.js
+++ osa8/library-frontend/src/components/UpdateBirthyear.js
@@ -1,12 +1,21 @@
-import { useMutation } from "@apollo/client"
+import { useMutation, useQuery } from "@apollo/client"
 import React, { useState } from "react"
 import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries"
+import Select from 'react-select'
 
 const UpdateBirthyear = () => {
     const [ name, setName ] = useState('')
     const [ setBornTo, setBorn ] = useState(0)
 
     const [ editAuthor ] = useMutation(EDIT_AUTHOR, { refetchQueries: [ { query: ALL_AUTHORS} ] })
+    const result = useQuery(ALL_AUTHORS)
+    const options = result.data.allAuthors.map(
+        author => ({ 
+            value: author.name, 
+            label: author.name, 
+            born: author.born !== null ? author.born : 0 
+        })
+    )
 
     const submit = (e) => {
         e.preventDefault()
@@ -15,13 +24,18 @@ const UpdateBirthyear = () => {
         setBorn('')
     }
 
+    const handleChange = (selectedOption) => {
+        setName(selectedOption.value)
+        setBorn(selectedOption.born)
+    }
+
     return (
         <div>
             <h4>set birthyear</h4>
             <form onSubmit={submit}>
             <div>
                 name
-                <input value={name} onChange={({ target }) => setName(target.value)} />
+                <Select defaultValue={name} options={options} onChange={handleChange} />
             </div>
             <div>
                 born
