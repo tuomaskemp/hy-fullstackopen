51be05c7db5e1fbcd246842cca6d3b691fd0a263
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2022-01-14 10:56:39 +0200

    complete 9.10.

--- osa9/patientor_backend/src/index.ts
+++ osa9/patientor_backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
 const app = express();
 import cors = require('cors');
 
@@ -8,9 +9,10 @@ app.use(express.json());
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
